ea65ffb7579d09adb7c07963d6f18f68cf89714c
mswjst <mswjst@gmail.com>
2019-12-25 00:25:17 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Speech Asistant App
+
+## Python Speech Asistant App with youtube - traversy media
+
+addition
+- [x] music search
+
+issue
+- [ ] sometimes, speech recognition is too slow 
+
+- [ ] it can be slow because of beautiful soup when search music
---
+++ main.py
@@ -0,0 +1,73 @@
+from time import ctime
+import speech_recognition as sr
+import webbrowser
+import time
+import os
+import random
+import pyttsx3
+import requests
+from bs4 import BeautifulSoup
+from csv import writer
+
+r = sr.Recognizer()
+
+speaker = pyttsx3.init('sapi5')
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', voices[1].id)
+rate = speaker.getProperty('rate')
+speaker.setProperty('rate', 173)
+
+
+def record_audio(ask=False):
+    with sr.Microphone() as source:
+        if ask:
+            alexis_speak(ask)
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            alexis_speak('Sorry, I did not get that')
+        except sr.RequestError:
+            alexis_speak('Sorry, my speech service is down')
+        return voice_data
+
+
+def alexis_speak(audio_string):
+    print('Computer: ' + audio_string)
+    speaker.say(audio_string)
+    speaker.runAndWait()
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        alexis_speak('My name is Alexis')
+    if 'what time is it' in voice_data:
+        alexis_speak(ctime())
+    if 'search' in voice_data:
+        search = record_audio('What do you want to search for?')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        alexis_speak('Here is what I found for ' + search)
+    if 'find location' in voice_data:
+        location = record_audio('What is the location?')
+        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        webbrowser.get().open(url)
+        alexis_speak('Here is the location of ' + location)
+    if 'music' in voice_data:
+        music = record_audio('What music do you want to listen to?')
+        print(music)
+        response = requests.get(
+            'https://www.youtube.com/results?search_query=' + music)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        posts = soup.findAll(class_='yt-lockup-title')[0].find('a')['href']
+        webbrowser.get().open('https://www.youtube.com/' + posts)
+    if 'exit' in voice_data:
+        exit()
+
+
+time.sleep(1)
+alexis_speak('How can I help you?')
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
